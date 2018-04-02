b7b992afb2620e3433675929c8e092c0c3094471
Javier Llopis <javier@llopis.me>
2018-04-02 12:01:20 +0100

    Add check for nobackup to inhibit running.

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 ldir=$( dirname $0 )
 cd $ldir
+if [ -f nobackup ]
+then
+    echo "File nobackup present. Not running."
+    exit 1
+fi
 source bin/activate
-python ./davbackup.py $@ 
+python ./davbackup.py $@
